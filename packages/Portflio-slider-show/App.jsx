9764567fd1e82487f59e8bce3f46b4545c6b038f
dengli <dengli@keytop.com.cn>
2023-11-07 23:17:36 +0800

    feat: 多层动画

--- packages/Portflio-slider-show/App.jsx
+++ packages/Portflio-slider-show/App.jsx
@@ -5,6 +5,7 @@ import { Experience } from './Experience';
 import { ScrollManager } from './ScrollManager';
 import { MotionConfig } from 'framer-motion';
 import { Introduce } from './Introduce';
+import { motion } from 'framer-motion';
 const framerMotionConfig = {
   duration: 0.5,
   type: 'string'
@@ -13,7 +14,6 @@ const framerMotionConfig = {
 function App() {
   const [section, setSection] = useState(0);
   const [menuOpened, setMenuOpened] = useState(false);
-
   return (
     <>
       <MotionConfig
@@ -26,31 +26,28 @@ function App() {
           className="z-20 fixed top-12 right-12 p-3 bg-indigo-600 w-11 h-11 rounded-md"
         >
           <div
-            className={`bg-white h-0.5 rounded-md w-full transition-all ${
-              menuOpened ? 'rotate-45  translate-y-0.5' : ''
-            }`}
+            className={`bg-white h-0.5 rounded-md w-full transition-all ${menuOpened ? 'rotate-45  translate-y-0.5' : ''
+              }`}
           />
           <div
-            className={`bg-white h-0.5 rounded-md w-full my-1 ${
-              menuOpened ? 'hidden' : ''
-            }`}
+            className={`bg-white h-0.5 rounded-md w-full my-1 ${menuOpened ? 'hidden' : ''
+              }`}
           />
           <div
-            className={`bg-white h-0.5 rounded-md w-full transition-all ${
-              menuOpened ? '-rotate-45' : ''
-            }`}
+            className={`bg-white h-0.5 rounded-md w-full transition-all ${menuOpened ? '-rotate-45' : ''
+              }`}
           />
         </button>
         <Canvas shadows camera={{ position: [0, 0, 10], fov: 42 }}>
           <color attach="background" args={['#e6e7ff']} />
           <ScrollControls pages={3} damping={0.1}>
             <ScrollManager
-             
+
               section={section}
               onSelectionChange={setSection}
             />
             <Scroll>
-              <Experience  menuOpened={menuOpened} animation="Flying" section={section} />
+              <Experience menuOpened={menuOpened} animation="Flying" section={section} />
             </Scroll>
             <Scroll html>
               <Introduce />
--- packages/Portflio-slider-show/Experience.jsx
+++ packages/Portflio-slider-show/Experience.jsx
@@ -1,18 +1,20 @@
 import {
   Environment,
   MeshDistortMaterial,
-  MeshWobbleMaterial
+  MeshWobbleMaterial,
+  useScroll
 } from '@react-three/drei';
 import { useMotionValue, animate } from 'framer-motion';
 
 import { Avatar } from './Avator';
 import { motion } from 'framer-motion-3d';
 import { useThree, useFrame } from '@react-three/fiber';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 export function Experience(props) {
-  const { animation, section, menuOpened } = props;
+  const { animation, menuOpened } = props;
   const { viewport } = useThree();
-
+  const data = useScroll();
+  const [section, setSection] = useState(0)
   const cameraPositionX = useMotionValue();
   const cameraLookAtX = useMotionValue();
   useEffect(() => {
@@ -23,7 +25,16 @@ export function Experience(props) {
   useFrame(state => {
     state.camera.position.x = cameraPositionX.get();
     state.camera.lookAt(cameraLookAtX.get(), 0, 0);
+
+    let curSelection = Math.floor(data.offset * data.pages);
+    if (curSelection > 2) {
+      curSelection = 2;
+    }
+    if (curSelection !== section) {
+      setSection(curSelection)
+    }
   });
+
   return (
     <>
       <ambientLight />
@@ -32,9 +43,34 @@ export function Experience(props) {
       <motion.group
         position={[0, 0, 0]}
         scale={[2, 2, 2]}
+        animate={section + ''}
+        variants={
+          {
+            0: {
+              y: -1.5
+            },
+            1: {
+              y: -viewport.height
+            },
+            2: {
+              y: -viewport.height * 2 + 0.5,
+              scaleX: 3,
+              scaleY: 3,
+              scaleZ: 3,
+              rotateY: -Math.PI / 4,
+            },
+          }
+        }
+      >
+        <Avatar animation={section === 0 ? 'Flying' : 'Standing'} />
+      </motion.group>
+
+
+      <motion.group
+        position={[0, 0, 0]}
+        scale={[2, 2, 2]}
         animate={{
-          x: 0,
-          y: section === 1 ? -viewport.height : -1.5
+          y: section === 1 ? -viewport.height : -1.5,
         }}
       >
         <mesh position-x={2} position-y={1}>
@@ -61,7 +97,7 @@ export function Experience(props) {
           <meshNormalMaterial />
           <boxGeometry />
         </mesh>
-        <Avatar animation={section === 0 ? 'Flying' : 'Standing'} />
+
       </motion.group>
     </>
   );
